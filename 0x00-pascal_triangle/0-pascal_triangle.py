d87bae37b22c5ef70db74175337adf3d3c2b47ac
Ousmane Konate <konatealim@gmail.com>
2023-03-23 17:03:07 +0100

    Implementing pascal_triangle function

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    result = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(result[i-1][j-1] + result[i-1][j])
+        row.append(1)
+        result.append(row)
+    return result
+
