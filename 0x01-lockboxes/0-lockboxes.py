242238f82119c2d45487f2da85cd3cd4e8ea5ce4
Ousmane Konate <konatealim@gmail.com>
2023-03-31 01:14:10 +0200

    add

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for the method canUnlockAll(boxes)"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened."""
+    if not boxes:
+        return False
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    return len(keys) == len(boxes)
