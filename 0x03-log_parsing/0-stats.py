f7e3f2143ea69378125890f66cc0040b35111423
Ousmane Konate <konatealim@gmail.com>
2023-04-14 03:13:27 +0200

    add task 0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""log parsing"""
+
+
+import sys
+
+
+def print_stats(file_size, status):
+    """Prints stats"""
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+status = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+counter = 0
+file_size = 0
+try:
+    for line in sys.stdin:
+        if counter == 10:
+            print_stats(file_size, status)
+            counter = 1
+        else:
+            counter += 1
+        line = line.split()
+        try:
+            file_size += int(line[-1])
+        except Exception as e:
+            pass
+        try:
+            for key, value in status.items():
+                if line[7] == key:
+                    status[key] += 1
+        except Exception as e:
+            pass
+    print_stats(file_size, status)
+except KeyboardInterrupt as e:
+    print_stats(file_size, status)
+    raise
