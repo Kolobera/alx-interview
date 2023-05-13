491446ce7618983e56f10132e3384f2c2ed7b744
Ousmane Konate <konatealim@gmail.com>
2023-05-13 13:27:35 +0200

    add rotate_matrix method

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Rotate Matrix module"""
+
+
+def rotate_2d_matrix(matrix):
+    """Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
+    n = len(matrix[0])
+    copy_m = [[matrix[i][j] for j in range(n)] for i in range (n)]
+    for i in range(n):
+        for j in range(n):
+            matrix[j][n-1-i] = copy_m[i][j]
