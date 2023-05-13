ef6037432289851edb5b03775449bd8fb03f33c0
Ousmane Konate <konatealim@gmail.com>
2023-05-13 13:31:08 +0200

    fixing style

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,7 +5,7 @@
 def rotate_2d_matrix(matrix):
     """Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
     n = len(matrix[0])
-    copy_m = [[matrix[i][j] for j in range(n)] for i in range (n)]
+    copy_m = [[matrix[i][j] for j in range(n)] for i in range(n)]
     for i in range(n):
         for j in range(n):
             matrix[j][n-1-i] = copy_m[i][j]
