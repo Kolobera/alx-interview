76db68a1adfbb5f38c4f6adb51c075e946ba60f3
Ousmane Konate <konatealim@gmail.com>
2023-05-23 17:00:24 +0200

    add Task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,16 +8,16 @@ def island_perimeter(grid):
     n = len(grid)
     perimeter = 0
     for i in range(n):
-        for j in range (m):
+        for j in range(m):
             if grid[i][j] == 1:
                 contour = 4
-                if grid[i + 1][j] == 1:
+                if i != n - 1 and grid[i + 1][j] == 1:
                     contour -= 1
-                if grid[i][j + 1] == 1:
+                if j != m - 1 and grid[i][j + 1] == 1:
                     contour -= 1
-                if grid[i][j - 1] == 1:
+                if j != 0 and grid[i][j - 1] == 1:
                     contour -= 1
-                if grid[i - 1][j] == 1:
+                if i != 0 and grid[i - 1][j] == 1:
                     contour -= 1
                 perimeter += contour
     return perimeter
