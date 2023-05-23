62028eb0c238bff26ff00da778bae95afaa98d2e
Ousmane Konate <konatealim@gmail.com>
2023-05-23 16:55:38 +0200

    add Task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,18 +7,17 @@ def island_perimeter(grid):
     m = len(grid[0])
     n = len(grid)
     perimeter = 0
-    line_tracker = []
-    column_tracker = []
     for i in range(n):
-        for j in range(m):
+        for j in range (m):
             if grid[i][j] == 1:
-                perimeter += 1
-                line_tracker.append(i)
-                column_tracker.append(j)
-    if perimeter == 0:
-        return 0
-    line_tracker = dict((x,line_tracker.count(x)) for x in line_tracker)
-    column_tracker = dict((x,column_tracker.count(x)) for x in column_tracker)
-    adder = len([i for i in line_tracker.values() if i > 1]) + len(
-        [i for i in column_tracker.values() if i > 1]) - 3
-    return 2 * (perimeter + 1 - adder)
+                contour = 4
+                if grid[i + 1][j] == 1:
+                    contour -= 1
+                if grid[i][j + 1] == 1:
+                    contour -= 1
+                if grid[i][j - 1] == 1:
+                    contour -= 1
+                if grid[i - 1][j] == 1:
+                    contour -= 1
+                perimeter += contour
+    return perimeter
